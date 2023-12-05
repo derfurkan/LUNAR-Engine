9fa55f7f122867a3370aaecfb900d493c6db1a99
derfurkan <arik_furkan@proton.me>
2023-12-05 17:01:16 +0100

    Create RandomManager.cs

---
+++ LUNAR-Engine/RandomManager.cs
@@ -0,0 +1,19 @@
+﻿namespace LUNAR_Engine
+{
+    public static class RandomManager
+    {
+
+        public static Random Random = new Random();
+
+        public static int GetRandomInt(int min, int max)
+        {
+            return Random.Next(min, max);
+        }
+
+        public static double GetRandomDouble(double min, double max)
+        {
+            return Random.NextDouble() * (max - min) + min;
+        }
+
+    }
+}
